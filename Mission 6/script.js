ea0f9c7a4253230dc4453569e76ed08a410d9a0d
MILLE-tech <arsene.caron1756@gmail.com>
2024-12-05 12:20:30 +0100

    Add files via upload

---
+++ Mission 6/script.js
@@ -0,0 +1,4 @@
+// JavaScript pour ajouter des fonctionnalités si nécessaire
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("La page est en cours de création. Merci de patienter.");
+});
